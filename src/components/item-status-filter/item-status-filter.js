6762ccef5c39226d8fdef2a26c7062e2e67b3c69
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2021-04-05 16:15:46 +0300

    add toggle buttons classes on click filter

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -3,17 +3,36 @@ import React, { Component } from 'react';
 import './item-status-filter.css';
 
 export default class ItemStatusFilter extends Component{
-  render(){
-    const { onFilterTodoItems } = this.props;
+  constructor(){
+    super();
 
+    this.filterClickHandler = (filterProp) => {
+      return ( event ) => {
+        const button = event.target;
+        const siblings = document.querySelectorAll(".btn-group .btn");
+
+        siblings.forEach((item) => {
+          item.className = "btn btn-outline-secondary";
+        });
+
+        button.className = "btn btn-info";
+        
+        const { onFilterTodoItems } = this.props;
+        onFilterTodoItems(filterProp)
+      }
+
+    };
+  };
+
+  render(){
     return (
       <div className="btn-group">
         <button type="button"
-                className="btn btn-info" onClick={() => onFilterTodoItems("all")}>All</button>
+                className="btn btn-info" onClick={this.filterClickHandler("all")}>All</button>
         <button type="button"
-                className="btn btn-outline-secondary" onClick={() => onFilterTodoItems("active")}>Active</button>
+                className="btn btn-outline-secondary" onClick={this.filterClickHandler("active")}>Active</button>
         <button type="button"
-                className="btn btn-outline-secondary" onClick={() => onFilterTodoItems("done")}>Done</button>
+                className="btn btn-outline-secondary" onClick={this.filterClickHandler("done")}>Done</button>
       </div>
     );
   }
