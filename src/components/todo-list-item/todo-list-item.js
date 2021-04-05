1dde9300ecac47fa7293e6eeb2566264368f944e
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2021-04-05 22:19:24 +0300

    add info btn to todo-list-item, fix styles

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -16,20 +16,29 @@ export default class TodoListItem extends Component{
       <span className={classList}>
         <span
           className="todo-list-item-label"
-          onClick={ onToggleDone }>
+          onClick={ onToggleDone }
+          title={label}>
           {label}
         </span>
-  
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onToggleImportant}>
+          <i className="fa fa-trash-o" />
+        </button>
+
+        <hr className="float-right" style={{height: "35px", width: "1px", margin: "0px 10px 0 10px" , backgroundColor: "rgba(0,0,0,.225)"}}/>
+
+        <button type="button"
+                className="btn btn-outline-primary btn-sm float-right"
+                onClick={onDeleted}>
+          <i className="fa fa-file" />
+        </button>
+
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
                 onClick={ onToggleImportant }>
           <i className="fa fa-exclamation" />
-        </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right"
-                onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
         </button>
       </span>
     );
