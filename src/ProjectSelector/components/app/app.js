0f42096302014f96d4042977ef04b6e5671a88f6
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2021-04-12 11:47:29 +0300

    add data to ProjectSelector option list

--- src/ProjectSelector/components/app/app.js
+++ src/ProjectSelector/components/app/app.js
@@ -6,21 +6,46 @@ import "./app.css"
 
 export default class ProjecSelector extends Component {
     render() {
+        const projectsData = [
+            {
+                id: 1,
+                label: "WSO"
+            },
+            {
+                id: 2,
+                label: "TAYX"
+            },
+            {
+                id: 3,
+                label: "SIGMA"
+            },
+            {
+                id: 4,
+                label: "KLM"
+            },
+            {
+                id: 5,
+                label: "TASK-MNGR"
+            }
+        ];
+
+        const projects = projectsData.map((item) => {
+            return (
+                <option key={item.id}>{item.label}</option>
+            )
+        });
+
         return (
             <div className="project-selector-wrapper">
                 <h1>Привет,</h1>
                 <h2>
                     выбери проект на сегодня
                 </h2>
-                <div class="form-group d-flex">
+                <div className="form-group d-flex">
                     <select className="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                        {projects}
                     </select>
-                    <Link to="/Manager">
+                    <Link to="/manager">
                         <button className="btn btn-primary ml-1">
                             Выбрать
                         </button>
